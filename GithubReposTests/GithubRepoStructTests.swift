dea1812031c6556764150e37b4c6caa0eed6937c
Rayan Saeed <rayancodes@gmail.com>
2022-06-15 16:48:59 +0500

    Setup Cocoapods

---
+++ GithubReposTests/GithubRepoStructTests.swift
@@ -0,0 +1,25 @@
+//
+//  GithubRepoStructTests.swift
+//  GithubReposTests
+//
+//  Created by Rayan Saeed on 15/06/2022.
+//
+
+import XCTest
+@testable import GithubRepos
+
+class GithubRepoStructTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+	}
+
+}
