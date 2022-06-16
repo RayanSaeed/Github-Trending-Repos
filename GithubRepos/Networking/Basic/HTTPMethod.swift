3d3bf498ed7d679f0f2bbc9ac46d9ae7ddf9c0c0
Rayan Saeed <rayancodes@gmail.com>
2022-06-16 10:02:24 +0500

    Add other methods in HTTP Method enum and add comments

--- GithubRepos/Networking/Basic/HTTPMethod.swift
+++ GithubRepos/Networking/Basic/HTTPMethod.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+/// HTTP request methods.
 enum HTTPMethod: String {
+
+	/// HTTP GET - to retrieve a resource
 	case get = "GET"
+
+	// For now we only need GET.
+	// But we can add other methods as well if needed as follows:
+
+	/// HTTP POST - to create a resource
+	case post = "POST"
+
+	/// HTTP PUT - to replace a resource
+	case put = "PUT"
+
+	/// HTTP PATCH - to update a resource
+	case patch = "PATCH"
+
+	/// HTTP DELETE - to delete a resource
+	case delete = "DELETE"
 }
