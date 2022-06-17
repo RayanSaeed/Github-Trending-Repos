f4cb4ba7a97ec5493f92fb1861246d76f9b1fe60
Rayan Saeed <rayancodes@gmail.com>
2022-06-17 18:45:41 +0500

    Add TrendingRepoViewController Tests

--- GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
+++ GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
@@ -30,4 +30,36 @@ class TrendingRepoViewControllerTests: XCTestCase {
 
 		expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
 	}
+
+	func testViewController_fetchData_fromViewModel_succeeds() async {
+		let mockService = GithubServiceSuccessfulMock()
+		let mockVM = TrendingReposViewModelSuccessfulMock(service: mockService)
+		let sut = await TrendingReposViewController(viewModel: mockVM)
+
+		_ = await sut.view
+
+		let viewModels = await sut.dataSource?.viewModels
+		let vm = viewModels?.first
+		let stub = RepositoryCellViewModel.stub
+
+		expect(viewModels?.count).to(equal(1))
+		expect(vm?.name).to(equal(stub.name))
+		expect(vm?.ownerLogin).to(equal(stub.ownerLogin))
+		expect(vm?.description).to(equal(stub.description))
+		expect(vm?.language).to(equal(stub.language))
+		expect(vm?.starsCount).to(equal(stub.starsCount))
+		expect(vm?.avatarUrl).to(equal(stub.avatarUrl))
+	}
+
+	func testViewController_fetchData_fromViewModel_fails() async {
+		let mockService = GithubServiceFailureMock()
+		let mockVM = TrendingReposViewModelFailureMock(service: mockService)
+		let sut = await TrendingReposViewController(viewModel: mockVM)
+
+		_ = await sut.view
+
+		let viewModels = await sut.dataSource?.viewModels
+
+		expect(viewModels).to(beNil())
+	}
 }
