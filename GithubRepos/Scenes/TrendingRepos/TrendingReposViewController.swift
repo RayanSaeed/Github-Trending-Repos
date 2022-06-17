f2fd8c9c72fa7514165183caf90419c63f1c7c96
Rayan Saeed <rayancodes@gmail.com>
2022-06-17 19:54:43 +0500

    Update TrendingReposViewController to pass the failing tests

--- GithubRepos/Scenes/TrendingRepos/TrendingReposViewController.swift
+++ GithubRepos/Scenes/TrendingRepos/TrendingReposViewController.swift
@@ -12,7 +12,16 @@ final class TrendingReposViewController: UIViewController {
 
 	// MARK: - Instance Properties
 	let viewModel: TrendingReposViewModelable
-	var dataSource: TrendingReposListDataSource<RepositoryCellViewModelable>?
+	var dataSource: TrendingReposListDataSource<RepositoryCellViewModelable>? {
+		didSet {
+			if dataSource == nil {
+				// show the lottie retry animation view
+			} else {
+				tableView.dataSource = dataSource
+				tableView.reloadData()
+			}
+		}
+	}
 
 	// MARK: - Private Properties
 
@@ -36,13 +45,20 @@ final class TrendingReposViewController: UIViewController {
         // Do any additional setup after loading the view.
 		self.title = viewModel.navigationBarTitle
 
+		setupTableView()
 		fetchTrendingRepos()
     }
 
 	// MARK: - Helper Methods
+	private func setupTableView() {
+		tableView.registerCell(type: RepositoryCell.self, identifier: RepositoryCell.reuseIdentifier)
+	}
+
 	private func fetchTrendingRepos() {
 		Task {
 			if let viewModels = await viewModel.getTrendingReposListViewModels() {
+				// We also need to keep a strong reference to the data source,
+				// since UITableView only uses a weak reference for it.
 				dataSource = .make(for: viewModels)
 			} else {
 				// Data could not be fetched from cache and service
