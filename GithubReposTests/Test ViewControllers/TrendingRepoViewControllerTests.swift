bda156a3bc6693db4f18fc73836079e0de7fb889
Rayan Saeed <rayancodes@gmail.com>
2022-06-18 18:06:12 +0500

    Refactor TrendingReposVC tests

--- GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
+++ GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
@@ -13,26 +13,25 @@ class TrendingRepoViewControllerTests: XCTestCase {
 
 	// MARK: - ViewController Tests
 
-	func testViewController_getsTitle_fromViewModel() {
+	func testViewController_getsTitle_fromViewModel() async {
 		let mockVM = TrendingReposViewModelSuccessfulMock()
-		let sut = TrendingReposViewController(viewModel: mockVM)
+		let sut = await makeSUT(with: mockVM)
 
-		_ = sut.view
-
-		let title = sut.title
+		let title = await sut.title
 
 		expect(title).to(equal(mockVM.navigationBarTitle))
 	}
 
 	func testViewController_fetchData_fromViewModel_succeeds() async {
+		// Given
 		let mockVM = TrendingReposViewModelSuccessfulMock()
-		let sut = await TrendingReposViewController(viewModel: mockVM)
+		let sut = await makeSUT(with: mockVM)
 
-		_ = await sut.view
-
+		// When
 		let indexPath = IndexPath(row: 0, section: 0)
 		let cell = await sut.tableView.cellForRow(at: indexPath) as? RepositoryCell
 
+		// Then
 		let numberOfRows = await sut.tableView.numberOfRows(inSection: 0)
 		let stub = RepositoryCellViewModel.stub
 
@@ -53,13 +52,44 @@ class TrendingRepoViewControllerTests: XCTestCase {
 
 	func testViewController_fetchData_fromViewModel_fails() async {
 		let mockVM = TrendingReposViewModelFailureMock()
-		let sut = await TrendingReposViewController(viewModel: mockVM)
-
-		_ = await sut.view
+		let sut = await makeSUT(with: mockVM)
 
 		let numberOfRows = await sut.tableView.numberOfRows(inSection: 0)
 
 		expect(numberOfRows).to(equal(0))
 		expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
+	}
+
+
+
+	func testViewController_expandsCell_whenTapped() async {
+		// Given
+		let sut = await makeSUT()
+
+		let tableView = await sut.tableView
+		let indexPath = IndexPath(row: 0, section: 0)
+		await tableView?.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+		await tableView?.delegate?.tableView?(tableView!, didSelectRowAt: indexPath)
+
+		// When
+		let cell = await tableView?.cellForRow(at: indexPath) as? RepositoryCell
+
+		let numberOfRows = await sut.tableView.numberOfRows(inSection: 0)
+		let isDetailStackViewHidden = await cell?.detailsStackView.isHidden
+		let isDescriptionLabelViewHidden = await cell?.descriptionLabel.isHidden
+
+		// Then
+		expect(numberOfRows).to(equal(1))
+		expect(isDetailStackViewHidden).to(beFalse())
+		expect(isDescriptionLabelViewHidden).to(beFalse())
+	}
+
+	// MARK: - Helpers
+	private func makeSUT(
+		with mockVM: TrendingReposViewModelable = TrendingReposViewModelSuccessfulMock()
+	) async -> TrendingReposViewController {
+		let sut = await TrendingReposViewController(viewModel: mockVM)
+		_ = await sut.view
+		return sut
 	}
 }
