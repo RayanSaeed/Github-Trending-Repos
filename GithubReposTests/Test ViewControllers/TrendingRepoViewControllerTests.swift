8660c0ee145dadfd8a12d665bbe6a8aee0937b14
Rayan Saeed <rayancodes@gmail.com>
2022-06-18 14:10:04 +0500

    Add and Refactor TrendingRepo VC tests

--- GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
+++ GithubReposTests/Test ViewControllers/TrendingRepoViewControllerTests.swift
@@ -11,62 +11,55 @@ import Nimble
 
 class TrendingRepoViewControllerTests: XCTestCase {
 
+	// MARK: - ViewController Tests
+
 	func testViewController_getsTitle_fromViewModel() {
-		let mockService = GithubServiceSuccessfulMock()
-		let mockVM = TrendingReposViewModelSuccessfulMock(service: mockService)
+		let mockVM = TrendingReposViewModelSuccessfulMock()
 		let sut = TrendingReposViewController(viewModel: mockVM)
 
 		_ = sut.view
 
-		expect(sut.title).to(equal(mockVM.navigationBarTitle))
-	}
+		let title = sut.title
 
-	func testViewController_fetchesData_fromViewModel() async {
-		let mockService = GithubServiceSuccessfulMock()
-		let mockVM = TrendingReposViewModelSuccessfulMock(service: mockService)
-		let sut = await TrendingReposViewController(viewModel: mockVM)
-
-		_ = await sut.view
-
-		DispatchQueue.main.async {
-			expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
-		}
+		expect(title).to(equal(mockVM.navigationBarTitle))
 	}
 
 	func testViewController_fetchData_fromViewModel_succeeds() async {
-		let mockService = GithubServiceSuccessfulMock()
-		let mockVM = TrendingReposViewModelSuccessfulMock(service: mockService)
+		let mockVM = TrendingReposViewModelSuccessfulMock()
 		let sut = await TrendingReposViewController(viewModel: mockVM)
-		let stub = RepositoryCellViewModel.stub
 
 		_ = await sut.view
 
-		let cell = await sut.tableView.cellForRow(at: .init(row: 0, section: 0)) as? RepositoryCell
+		let indexPath = IndexPath(row: 0, section: 0)
+		let cell = await sut.tableView.cellForRow(at: indexPath) as? RepositoryCell
+
 		let numberOfRows = await sut.tableView.numberOfRows(inSection: 0)
+		let stub = RepositoryCellViewModel.stub
+
+		let repoName = await cell?.repositoryNameLabel.text
+		let ownerName = await cell?.ownerNameLabel.text
+		let description = await cell?.descriptionLabel.text
+		let language = await cell?.languageLabel.text
+		let starsCount = await cell?.starsCountLabel.text
 
 		expect(numberOfRows).to(equal(1))
-
-		DispatchQueue.main.async {
-			expect(cell?.repositoryNameLabel.text).to(equal(stub.name))
-			expect(cell?.ownerNameLabel.text).to(equal(stub.ownerLogin))
-			expect(cell?.descriptionLabel.text).to(equal(stub.description))
-			expect(cell?.languageLabel.text).to(equal(stub.language))
-			expect(cell?.starsCountLabel.text).to(equal(stub.starsCount))
-		}
+		expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
+		expect(repoName).to(equal(stub.name))
+		expect(ownerName).to(equal(stub.ownerLogin))
+		expect(description).to(equal(stub.description))
+		expect(language).to(equal(stub.language))
+		expect(starsCount).to(equal(stub.starsCount))
 	}
 
 	func testViewController_fetchData_fromViewModel_fails() async {
-		let mockService = GithubServiceFailureMock()
-		let mockVM = TrendingReposViewModelFailureMock(service: mockService)
+		let mockVM = TrendingReposViewModelFailureMock()
 		let sut = await TrendingReposViewController(viewModel: mockVM)
 
 		_ = await sut.view
 
 		let numberOfRows = await sut.tableView.numberOfRows(inSection: 0)
 
-		DispatchQueue.main.async {
-			expect(numberOfRows).to(equal(0))
-			expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
-		}
+		expect(numberOfRows).to(equal(0))
+		expect(mockVM.fetchTrendingReposWasCalled).to(beTrue())
 	}
 }
